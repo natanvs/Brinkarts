d0de19f39a9ecdcefc4e389508d52c1d302e2d79
natan viana <natanvianasoares7@gmail.com>
2024-07-17 11:23:39 -0300

    concluido

---
+++ script.js
@@ -0,0 +1,20 @@
+function togglemode(){
+    const html = document.documentElement
+ 
+   if(html.classList.contains('light')) {
+     html.classList.remove('light')
+   } else {
+     html.classList.add('light')
+   } 
+ 
+ const img = document.querySelector("#profile img")
+ 
+   if(html.classList.contains('light')) {
+ 
+     img.setAttribute('src', './img/avatar.png')
+     
+   } else {
+ 
+     img.setAttribute('src', "./img/avatar-light.png")
+   } 
+ }
